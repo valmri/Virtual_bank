c56739ccbe655deb64b974b30ebd4aa2d3d7c342
marmie <valentin.marmie@outlook.com>
2021-12-29 19:16:56 +0100

    Changement icone du menu

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,10 +1,14 @@
 /* Menu responsive */
+var iconMenu = document.getElementById('btn_ouverture');
+
 function menu() {
     var leMenu = document.getElementById('leMenu');
     
     if(leMenu.classList.contains('menu_ferme') == true ) {
         leMenu.classList.replace('menu_ferme','menu_ouvert');
+        iconMenu.childNodes.item(1).classList.replace("la-bars", "la-times")
     } else {
         leMenu.classList.replace('menu_ouvert', 'menu_ferme');
+        iconMenu.childNodes.item(1).classList.replace("la-times", "la-bars")
     }
 }
